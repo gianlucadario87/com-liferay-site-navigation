brianchandotcom/liferay-portal

LPS-64056 Apply everywhere else

modules/apps/web-experience/site-navigation/site-navigation-breadcrumb-web/src/main/java/com/liferay/site/navigation/breadcrumb/web/portlet/template/SiteNavigationBreadcrumbPortletDisplayTemplateHandler.java
@@ -14,8 +14,7 @@
 
 package com.liferay.site.navigation.breadcrumb.web.portlet.template;
 
-import aQute.bnd.annotation.metatype.Configurable;
-
+import com.liferay.portal.configuration.ConfigurableUtil;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.portletdisplaytemplate.BasePortletDisplayTemplateHandler;
 import com.liferay.portal.kernel.servlet.taglib.ui.BreadcrumbEntry;
@@ -128,7 +127,7 @@ public class SiteNavigationBreadcrumbPortletDisplayTemplateHandler
 	@Modified
 	protected void activate(Map<String, Object> properties) {
 		_siteNavigationBreadcrumbWebTemplateConfiguration =
-			Configurable.createConfigurable(
+			ConfigurableUtil.createConfigurable(
 				SiteNavigationBreadcrumbWebTemplateConfiguration.class,
 				properties);
 	}

modules/apps/web-experience/site-navigation/site-navigation-language-web/src/main/java/com/liferay/site/navigation/language/web/portlet/template/SiteNavigationLanguagePortletDisplayTemplateHandler.java
@@ -14,8 +14,7 @@
 
 package com.liferay.site.navigation.language.web.portlet.template;
 
-import aQute.bnd.annotation.metatype.Configurable;
-
+import com.liferay.portal.configuration.ConfigurableUtil;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.portletdisplaytemplate.BasePortletDisplayTemplateHandler;
 import com.liferay.portal.kernel.servlet.taglib.ui.LanguageEntry;
@@ -103,7 +102,7 @@ public class SiteNavigationLanguagePortletDisplayTemplateHandler
 	@Modified
 	protected void activate(Map<String, Object> properties) {
 		_siteNavigationLanguageWebTemplateConfiguration =
-			Configurable.createConfigurable(
+			ConfigurableUtil.createConfigurable(
 				SiteNavigationLanguageWebTemplateConfiguration.class,
 				properties);
 	}

modules/apps/web-experience/site-navigation/site-navigation-menu-web/src/main/java/com/liferay/site/navigation/menu/web/portlet/template/SiteNavigationMenuPortletDisplayTemplateHandler.java
@@ -14,8 +14,7 @@
 
 package com.liferay.site.navigation.menu.web.portlet.template;
 
-import aQute.bnd.annotation.metatype.Configurable;
-
+import com.liferay.portal.configuration.ConfigurableUtil;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -129,7 +128,7 @@ public class SiteNavigationMenuPortletDisplayTemplateHandler
 	@Modified
 	protected void activate(Map<String, Object> properties) {
 		_siteNavigationMenuWebTemplateConfiguration =
-			Configurable.createConfigurable(
+			ConfigurableUtil.createConfigurable(
 				SiteNavigationMenuWebTemplateConfiguration.class, properties);
 	}
 

gianlucadario87/com-liferay-site-navigation

site-navigation-breadcrumb-web/src/main/java/com/liferay/site/navigation/breadcrumb/web/portlet/template/SiteNavigationBreadcrumbPortletDisplayTemplateHandler.java
@@ -14,8 +14,7 @@
 
 package com.liferay.site.navigation.breadcrumb.web.portlet.template;
 
-import aQute.bnd.annotation.metatype.Configurable;
-
+import com.liferay.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.portletdisplaytemplate.BasePortletDisplayTemplateHandler;
 import com.liferay.portal.kernel.servlet.taglib.ui.BreadcrumbEntry;
@@ -128,7 +127,7 @@ public class SiteNavigationBreadcrumbPortletDisplayTemplateHandler
 	@Modified
 	protected void activate(Map<String, Object> properties) {
 		_siteNavigationBreadcrumbWebTemplateConfiguration =
-			Configurable.createConfigurable(
+			ConfigurableUtil.createConfigurable(
 				SiteNavigationBreadcrumbWebTemplateConfiguration.class,
 				properties);
 	}

site-navigation-language-web/src/main/java/com/liferay/site/navigation/language/web/portlet/template/SiteNavigationLanguagePortletDisplayTemplateHandler.java
@@ -14,8 +14,7 @@
 
 package com.liferay.site.navigation.language.web.portlet.template;
 
-import aQute.bnd.annotation.metatype.Configurable;
-
+import com.liferay.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.portletdisplaytemplate.BasePortletDisplayTemplateHandler;
 import com.liferay.portal.kernel.servlet.taglib.ui.LanguageEntry;
@@ -103,7 +102,7 @@ public class SiteNavigationLanguagePortletDisplayTemplateHandler
 	@Modified
 	protected void activate(Map<String, Object> properties) {
 		_siteNavigationLanguageWebTemplateConfiguration =
-			Configurable.createConfigurable(
+			ConfigurableUtil.createConfigurable(
 				SiteNavigationLanguageWebTemplateConfiguration.class,
 				properties);
 	}

site-navigation-menu-web/src/main/java/com/liferay/site/navigation/menu/web/portlet/template/SiteNavigationMenuPortletDisplayTemplateHandler.java
@@ -14,8 +14,7 @@
 
 package com.liferay.site.navigation.menu.web.portlet.template;
 
-import aQute.bnd.annotation.metatype.Configurable;
-
+import com.liferay.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -129,7 +128,7 @@ public class SiteNavigationMenuPortletDisplayTemplateHandler
 	@Modified
 	protected void activate(Map<String, Object> properties) {
 		_siteNavigationMenuWebTemplateConfiguration =
-			Configurable.createConfigurable(
+			ConfigurableUtil.createConfigurable(
 				SiteNavigationMenuWebTemplateConfiguration.class, properties);
 	}
 