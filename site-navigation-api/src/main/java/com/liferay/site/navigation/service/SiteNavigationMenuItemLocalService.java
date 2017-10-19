jkappler/liferay-portal

LPS-75021 Regenerate service code

modules/apps/web-experience/site-navigation/site-navigation-api/src/main/java/com/liferay/site/navigation/service/SiteNavigationMenuItemLocalService.java
@@ -228,6 +228,10 @@ public interface SiteNavigationMenuItemLocalService extends BaseLocalService,
 	public List<SiteNavigationMenuItem> getSiteNavigationMenuItems(
 		long siteNavigationMenuId);
 
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public List<SiteNavigationMenuItem> getSiteNavigationMenuItemsByParent(
+		long parentSiteNavigationMenuItemId);
+
 	/**
 	* Returns the number of site navigation menu items.
 	*

modules/apps/web-experience/site-navigation/site-navigation-api/src/main/java/com/liferay/site/navigation/service/SiteNavigationMenuItemLocalServiceUtil.java
@@ -255,6 +255,12 @@ public class SiteNavigationMenuItemLocalServiceUtil {
 		return getService().getSiteNavigationMenuItems(siteNavigationMenuId);
 	}
 
+	public static java.util.List<com.liferay.site.navigation.model.SiteNavigationMenuItem> getSiteNavigationMenuItemsByParent(
+		long parentSiteNavigationMenuItemId) {
+		return getService()
+				   .getSiteNavigationMenuItemsByParent(parentSiteNavigationMenuItemId);
+	}
+
 	/**
 	* Returns the number of site navigation menu items.
 	*

modules/apps/web-experience/site-navigation/site-navigation-api/src/main/java/com/liferay/site/navigation/service/SiteNavigationMenuItemLocalServiceWrapper.java
@@ -269,6 +269,12 @@ public class SiteNavigationMenuItemLocalServiceWrapper
 		return _siteNavigationMenuItemLocalService.getSiteNavigationMenuItems(siteNavigationMenuId);
 	}
 
+	@Override
+	public java.util.List<com.liferay.site.navigation.model.SiteNavigationMenuItem> getSiteNavigationMenuItemsByParent(
+		long parentSiteNavigationMenuItemId) {
+		return _siteNavigationMenuItemLocalService.getSiteNavigationMenuItemsByParent(parentSiteNavigationMenuItemId);
+	}
+
 	/**
 	* Returns the number of site navigation menu items.
 	*

gianlucadario87/com-liferay-site-navigation

site-navigation-api/src/main/java/com/liferay/site/navigation/service/SiteNavigationMenuItemLocalService.java
@@ -184,6 +184,10 @@ public interface SiteNavigationMenuItemLocalService extends BaseLocalService,
 	public ActionableDynamicQuery getActionableDynamicQuery();
 
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+	public List<SiteNavigationMenuItem> getChildSiteNavigationMenuItems(
+		long parentSiteNavigationMenuItemId);
+
+	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public IndexableActionableDynamicQuery getIndexableActionableDynamicQuery();
 
 	/**

site-navigation-api/src/main/java/com/liferay/site/navigation/service/SiteNavigationMenuItemLocalServiceUtil.java
@@ -202,6 +202,12 @@ public class SiteNavigationMenuItemLocalServiceUtil {
 		return getService().getActionableDynamicQuery();
 	}
 
+	public static java.util.List<com.liferay.site.navigation.model.SiteNavigationMenuItem> getChildSiteNavigationMenuItems(
+		long parentSiteNavigationMenuItemId) {
+		return getService()
+				   .getChildSiteNavigationMenuItems(parentSiteNavigationMenuItemId);
+	}
+
 	public static com.liferay.portal.kernel.dao.orm.IndexableActionableDynamicQuery getIndexableActionableDynamicQuery() {
 		return getService().getIndexableActionableDynamicQuery();
 	}

site-navigation-api/src/main/java/com/liferay/site/navigation/service/SiteNavigationMenuItemLocalServiceWrapper.java
@@ -210,6 +210,12 @@ public class SiteNavigationMenuItemLocalServiceWrapper
 	}
 
 	@Override
+	public java.util.List<com.liferay.site.navigation.model.SiteNavigationMenuItem> getChildSiteNavigationMenuItems(
+		long parentSiteNavigationMenuItemId) {
+		return _siteNavigationMenuItemLocalService.getChildSiteNavigationMenuItems(parentSiteNavigationMenuItemId);
+	}
+
+	@Override
 	public com.liferay.portal.kernel.dao.orm.IndexableActionableDynamicQuery getIndexableActionableDynamicQuery() {
 		return _siteNavigationMenuItemLocalService.getIndexableActionableDynamicQuery();
 	}