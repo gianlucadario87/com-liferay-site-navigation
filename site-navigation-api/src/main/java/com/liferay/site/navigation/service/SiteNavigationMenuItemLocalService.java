pavel-savinov/liferay-portal

LPS-75887 Regenerate service code

modules/apps/web-experience/site-navigation/site-navigation-api/src/main/java/com/liferay/site/navigation/service/SiteNavigationMenuItemLocalService.java
@@ -244,6 +244,10 @@ public interface SiteNavigationMenuItemLocalService extends BaseLocalService,
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public int getSiteNavigationMenuItemsCount();
 
+	public SiteNavigationMenuItem updateSiteNavigationMenuItem(
+		long siteNavigationMenuId, long parentSiteNavigationMenuItemId,
+		int order, ServiceContext serviceContext) throws PortalException;
+
 	public SiteNavigationMenuItem updateSiteNavigationMenuItem(long userId,
 		long siteNavigationMenuItemId, long parentSiteNavigationMenuItemId,
 		ServiceContext serviceContext) throws PortalException;

modules/apps/web-experience/site-navigation/site-navigation-api/src/main/java/com/liferay/site/navigation/service/SiteNavigationMenuItemLocalServiceUtil.java
@@ -278,6 +278,16 @@ public class SiteNavigationMenuItemLocalServiceUtil {
 	}
 
 	public static com.liferay.site.navigation.model.SiteNavigationMenuItem updateSiteNavigationMenuItem(
+		long siteNavigationMenuId, long parentSiteNavigationMenuItemId,
+		int order,
+		com.liferay.portal.kernel.service.ServiceContext serviceContext)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return getService()
+				   .updateSiteNavigationMenuItem(siteNavigationMenuId,
+			parentSiteNavigationMenuItemId, order, serviceContext);
+	}
+
+	public static com.liferay.site.navigation.model.SiteNavigationMenuItem updateSiteNavigationMenuItem(
 		long userId, long siteNavigationMenuItemId,
 		long parentSiteNavigationMenuItemId,
 		com.liferay.portal.kernel.service.ServiceContext serviceContext)

modules/apps/web-experience/site-navigation/site-navigation-api/src/main/java/com/liferay/site/navigation/service/SiteNavigationMenuItemLocalServiceWrapper.java
@@ -294,6 +294,16 @@ public class SiteNavigationMenuItemLocalServiceWrapper
 
 	@Override
 	public com.liferay.site.navigation.model.SiteNavigationMenuItem updateSiteNavigationMenuItem(
+		long siteNavigationMenuId, long parentSiteNavigationMenuItemId,
+		int order,
+		com.liferay.portal.kernel.service.ServiceContext serviceContext)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return _siteNavigationMenuItemLocalService.updateSiteNavigationMenuItem(siteNavigationMenuId,
+			parentSiteNavigationMenuItemId, order, serviceContext);
+	}
+
+	@Override
+	public com.liferay.site.navigation.model.SiteNavigationMenuItem updateSiteNavigationMenuItem(
 		long userId, long siteNavigationMenuItemId,
 		long parentSiteNavigationMenuItemId,
 		com.liferay.portal.kernel.service.ServiceContext serviceContext)

gianlucadario87/com-liferay-site-navigation

site-navigation-api/src/main/java/com/liferay/site/navigation/service/SiteNavigationMenuItemLocalService.java
@@ -244,6 +244,10 @@ public interface SiteNavigationMenuItemLocalService extends BaseLocalService,
 	@Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
 	public int getSiteNavigationMenuItemsCount();
 
+	public SiteNavigationMenuItem updateSiteNavigationMenuItem(
+		long siteNavigationMenuItemId, long parentSiteNavigationMenuItemId,
+		int order, ServiceContext serviceContext) throws PortalException;
+
 	public SiteNavigationMenuItem updateSiteNavigationMenuItem(long userId,
 		long siteNavigationMenuItemId, long parentSiteNavigationMenuItemId,
 		ServiceContext serviceContext) throws PortalException;

site-navigation-api/src/main/java/com/liferay/site/navigation/service/SiteNavigationMenuItemLocalServiceUtil.java
@@ -278,6 +278,16 @@ public class SiteNavigationMenuItemLocalServiceUtil {
 	}
 
 	public static com.liferay.site.navigation.model.SiteNavigationMenuItem updateSiteNavigationMenuItem(
+		long siteNavigationMenuItemId, long parentSiteNavigationMenuItemId,
+		int order,
+		com.liferay.portal.kernel.service.ServiceContext serviceContext)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return getService()
+				   .updateSiteNavigationMenuItem(siteNavigationMenuItemId,
+			parentSiteNavigationMenuItemId, order, serviceContext);
+	}
+
+	public static com.liferay.site.navigation.model.SiteNavigationMenuItem updateSiteNavigationMenuItem(
 		long userId, long siteNavigationMenuItemId,
 		long parentSiteNavigationMenuItemId,
 		com.liferay.portal.kernel.service.ServiceContext serviceContext)

site-navigation-api/src/main/java/com/liferay/site/navigation/service/SiteNavigationMenuItemLocalServiceWrapper.java
@@ -294,6 +294,16 @@ public class SiteNavigationMenuItemLocalServiceWrapper
 
 	@Override
 	public com.liferay.site.navigation.model.SiteNavigationMenuItem updateSiteNavigationMenuItem(
+		long siteNavigationMenuItemId, long parentSiteNavigationMenuItemId,
+		int order,
+		com.liferay.portal.kernel.service.ServiceContext serviceContext)
+		throws com.liferay.portal.kernel.exception.PortalException {
+		return _siteNavigationMenuItemLocalService.updateSiteNavigationMenuItem(siteNavigationMenuItemId,
+			parentSiteNavigationMenuItemId, order, serviceContext);
+	}
+
+	@Override
+	public com.liferay.site.navigation.model.SiteNavigationMenuItem updateSiteNavigationMenuItem(
 		long userId, long siteNavigationMenuItemId,
 		long parentSiteNavigationMenuItemId,
 		com.liferay.portal.kernel.service.ServiceContext serviceContext)