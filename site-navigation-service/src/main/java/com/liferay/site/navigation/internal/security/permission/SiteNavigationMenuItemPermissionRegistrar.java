shuyangzhou/liferay-portal

LPS-75306 Implement in site-navigation.

modules/apps/web-experience/site-navigation/site-navigation-service/src/main/java/com/liferay/site/navigation/internal/permission/SiteNavigationMenuItemPermissionRegistrar.java
@@ -1,0 +1,75 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.navigation.internal.permission;
+
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionFactory;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.site.navigation.constants.SiteNavigationConstants;
+import com.liferay.site.navigation.model.SiteNavigationMenuItem;
+import com.liferay.site.navigation.service.SiteNavigationMenuItemLocalService;
+
+import java.util.Dictionary;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Preston Crary
+ */
+@Component(immediate = true)
+public class SiteNavigationMenuItemPermissionRegistrar {
+
+	@Activate
+	public void activate(BundleContext bundleContext) {
+		Dictionary<String, Object> properties = new HashMapDictionary<>();
+
+		properties.put(
+			"model.class.name", SiteNavigationMenuItem.class.getName());
+
+		_serviceRegistration = bundleContext.registerService(
+			ModelResourcePermission.class,
+			ModelResourcePermissionFactory.create(
+				SiteNavigationMenuItem.class,
+				SiteNavigationMenuItem::getSiteNavigationMenuItemId,
+				_siteNavigationMenuItemLocalService::getSiteNavigationMenuItem,
+				_portletResourcePermission,
+				(modelResourcePermission, logicConsumer) -> {
+				}),
+			properties);
+	}
+
+	@Deactivate
+	public void deactivate() {
+		_serviceRegistration.unregister();
+	}
+
+	@Reference(
+		target = "(resource.name=" + SiteNavigationConstants.RESOURCE_NAME + ")"
+	)
+	private PortletResourcePermission _portletResourcePermission;
+
+	private ServiceRegistration<ModelResourcePermission> _serviceRegistration;
+
+	@Reference
+	private SiteNavigationMenuItemLocalService
+		_siteNavigationMenuItemLocalService;
+
+}

modules/apps/web-experience/site-navigation/site-navigation-service/src/main/java/com/liferay/site/navigation/internal/permission/SiteNavigationMenuPermissionRegistrar.java
@@ -1,0 +1,73 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.navigation.internal.permission;
+
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionFactory;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.site.navigation.constants.SiteNavigationConstants;
+import com.liferay.site.navigation.model.SiteNavigationMenu;
+import com.liferay.site.navigation.service.SiteNavigationMenuLocalService;
+
+import java.util.Dictionary;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Preston Crary
+ */
+@Component(immediate = true)
+public class SiteNavigationMenuPermissionRegistrar {
+
+	@Activate
+	public void activate(BundleContext bundleContext) {
+		Dictionary<String, Object> properties = new HashMapDictionary<>();
+
+		properties.put("model.class.name", SiteNavigationMenu.class.getName());
+
+		_serviceRegistration = bundleContext.registerService(
+			ModelResourcePermission.class,
+			ModelResourcePermissionFactory.create(
+				SiteNavigationMenu.class,
+				SiteNavigationMenu::getSiteNavigationMenuId,
+				_siteNavigationMenuLocalService::getSiteNavigationMenu,
+				_portletResourcePermission,
+				(modelResourcePermission, logicConsumer) -> {
+				}),
+			properties);
+	}
+
+	@Deactivate
+	public void deactivate() {
+		_serviceRegistration.unregister();
+	}
+
+	@Reference(
+		target = "(resource.name=" + SiteNavigationConstants.RESOURCE_NAME + ")"
+	)
+	private PortletResourcePermission _portletResourcePermission;
+
+	private ServiceRegistration<ModelResourcePermission> _serviceRegistration;
+
+	@Reference
+	private SiteNavigationMenuLocalService _siteNavigationMenuLocalService;
+
+}

gianlucadario87/com-liferay-site-navigation

site-navigation-service/src/main/java/com/liferay/site/navigation/internal/security/permission/SiteNavigationMenuItemPermissionRegistrar.java
@@ -1,0 +1,75 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.navigation.internal.security.permission;
+
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionFactory;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.site.navigation.constants.SiteNavigationConstants;
+import com.liferay.site.navigation.model.SiteNavigationMenuItem;
+import com.liferay.site.navigation.service.SiteNavigationMenuItemLocalService;
+
+import java.util.Dictionary;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Preston Crary
+ */
+@Component(immediate = true)
+public class SiteNavigationMenuItemPermissionRegistrar {
+
+	@Activate
+	public void activate(BundleContext bundleContext) {
+		Dictionary<String, Object> properties = new HashMapDictionary<>();
+
+		properties.put(
+			"model.class.name", SiteNavigationMenuItem.class.getName());
+
+		_serviceRegistration = bundleContext.registerService(
+			ModelResourcePermission.class,
+			ModelResourcePermissionFactory.create(
+				SiteNavigationMenuItem.class,
+				SiteNavigationMenuItem::getSiteNavigationMenuItemId,
+				_siteNavigationMenuItemLocalService::getSiteNavigationMenuItem,
+				_portletResourcePermission,
+				(modelResourcePermission, consumer) -> {
+				}),
+			properties);
+	}
+
+	@Deactivate
+	public void deactivate() {
+		_serviceRegistration.unregister();
+	}
+
+	@Reference(
+		target = "(resource.name=" + SiteNavigationConstants.RESOURCE_NAME + ")"
+	)
+	private PortletResourcePermission _portletResourcePermission;
+
+	private ServiceRegistration<ModelResourcePermission> _serviceRegistration;
+
+	@Reference
+	private SiteNavigationMenuItemLocalService
+		_siteNavigationMenuItemLocalService;
+
+}

site-navigation-service/src/main/java/com/liferay/site/navigation/internal/security/permission/SiteNavigationMenuPermissionRegistrar.java
@@ -1,0 +1,73 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.navigation.internal.security.permission;
+
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionFactory;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.site.navigation.constants.SiteNavigationConstants;
+import com.liferay.site.navigation.model.SiteNavigationMenu;
+import com.liferay.site.navigation.service.SiteNavigationMenuLocalService;
+
+import java.util.Dictionary;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Preston Crary
+ */
+@Component(immediate = true)
+public class SiteNavigationMenuPermissionRegistrar {
+
+	@Activate
+	public void activate(BundleContext bundleContext) {
+		Dictionary<String, Object> properties = new HashMapDictionary<>();
+
+		properties.put("model.class.name", SiteNavigationMenu.class.getName());
+
+		_serviceRegistration = bundleContext.registerService(
+			ModelResourcePermission.class,
+			ModelResourcePermissionFactory.create(
+				SiteNavigationMenu.class,
+				SiteNavigationMenu::getSiteNavigationMenuId,
+				_siteNavigationMenuLocalService::getSiteNavigationMenu,
+				_portletResourcePermission,
+				(modelResourcePermission, consumer) -> {
+				}),
+			properties);
+	}
+
+	@Deactivate
+	public void deactivate() {
+		_serviceRegistration.unregister();
+	}
+
+	@Reference(
+		target = "(resource.name=" + SiteNavigationConstants.RESOURCE_NAME + ")"
+	)
+	private PortletResourcePermission _portletResourcePermission;
+
+	private ServiceRegistration<ModelResourcePermission> _serviceRegistration;
+
+	@Reference
+	private SiteNavigationMenuLocalService _siteNavigationMenuLocalService;
+
+}